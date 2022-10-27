bdcac9a891023bc7e9de91c03e72eee3363523d7
Jan Christophersen <code@chrstphrsn.cc>
2022-10-27 16:14:43 +0200

    Move IconButton to separate file

--- trello/views/RightSidebarView.swift
+++ trello/views/RightSidebarView.swift
@@ -7,26 +7,6 @@
 
 import SwiftUI
 
-struct IconButton: ButtonStyle {
-    var icon: String
-    
-    @State var hovering: Bool = false
-    
-    func makeBody(configuration: Configuration) -> some View {
-        Image(systemName: self.icon)
-            .font(.system(size: 24))
-            .padding(4)
-            .background(self.hovering ? Color("CardBg") : Color("CardBg").opacity(0.8))
-            .foregroundColor(.white)
-            .cornerRadius(8)
-            .scaleEffect(configuration.isPressed ? 1.2 : 1)
-            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
-            .onHover { hover in
-                self.hovering = hover
-            }
-    }
-}
-
 struct RightSidebarView: View {
     var doneList: Binding<List>?;
     
